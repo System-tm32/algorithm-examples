61e088964537d2002f86af2b976111477b7101bc
Kirill Ilin <ilin@uchi.ru>
2021-11-29 21:26:12 +0300

    feat: add binarySearch using while

--- src/components/BinarySearch/BinarySearch.tsx
+++ src/components/BinarySearch/BinarySearch.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { binarySearch } from './functions';
+import { binarySearch } from './Functions';
 
 const BinarySearch = () => {
-  return <div>result:{binarySearch()}</div>;
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16];
+  return <div>result:{binarySearch(6, numbers)}</div>;
 };
 
 export { BinarySearch };
--- src/components/BinarySearch/Functions.ts
+++ src/components/BinarySearch/Functions.ts
@@ -1,3 +1,13 @@
-export const binarySearch = () => {
-  return 1;
+export const binarySearch = (item: number, list: number[]): Number | null => {
+  let low = 0;
+  let hight = list.length;
+
+  while (low <= hight) {
+    const mid = Math.floor((low + hight) / 2);
+    const guess = list[mid];
+    if (guess == item) return mid;
+    if (guess > item) hight = mid - 1;
+    else low = mid + 1;
+  }
+  return null;
 };
