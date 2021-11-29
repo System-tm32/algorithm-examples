7cf6c8438072e020684753fab6bf026323c256ee
Kirill Ilin <ilin@uchi.ru>
2021-11-29 21:48:34 +0300

    feat: using recursion

--- src/components/BinarySearch/BinarySearch.tsx
+++ src/components/BinarySearch/BinarySearch.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { binarySearch } from './Functions';
+import { binarySearch, binarySearch1 } from './Functions';
 
 const BinarySearch = () => {
   const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16];
-  return <div>result:{binarySearch(6, numbers)}</div>;
+  return <div>result:{binarySearch1(6, numbers)}</div>;
 };
 
 export { BinarySearch };
--- src/components/BinarySearch/Functions.ts
+++ src/components/BinarySearch/Functions.ts
@@ -11,3 +11,14 @@ export const binarySearch = (item: number, list: number[]): Number | null => {
   }
   return null;
 };
+
+export const binarySearch1 = (item: number, list: number[]): number | null => {
+  if (list.length == 1) {
+    return list[0];
+  }
+  const mid = Math.floor(list.length / 2);
+  const guess = list[mid];
+  if (guess === item) return mid;
+  if (guess > item) return binarySearch1(item, list.slice(0, mid));
+  else return binarySearch1(item, list.slice(mid, list.length));
+};
