1d4b2d7f17c25e681c630958a8cd7cdf2fb6ae84
Kirill Ilin <ilin@uchi.ru>
2021-12-12 14:46:51 +0300

    refactor: add types

--- src/components/Approximate/Approximate.tsx
+++ src/components/Approximate/Approximate.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Approximate = () => {
-  let statesNeeded = new Set(['mt', 'wa', 'or', 'id', 'nv', 'ut', 'ca', 'az']);
+  let statesNeeded = new Set<string>([
+    'mt',
+    'wa',
+    'or',
+    'id',
+    'nv',
+    'ut',
+    'ca',
+    'az',
+  ]);
 
   const stations = {
     kone: new Set(['id', 'nv', 'ut']),
@@ -15,18 +24,22 @@ const Approximate = () => {
 
   while (statesNeeded.size != 0) {
     let bestStation = null;
-    let statesCovered = new Set();
-    Object.keys(stations).forEach((value) => {
-      const covered = intersection(statesNeeded, stations[value]);
+    let statesCovered = new Set<string>();
+
+    type Tstations = keyof typeof stations;
+    Object.keys(stations).forEach((value: string) => {
+      const stationsIndexType = value as keyof typeof stations;
+
+      const covered = intersection(statesNeeded, stations[stationsIndexType]);
       if (covered.size > statesCovered.size) {
-        bestStation = stations[value];
+        bestStation = stations[stationsIndexType];
         statesCovered = covered;
       }
     });
-    statesNeeded = difference(statesCovered, statesNeeded);
+    statesNeeded = difference<string>(statesCovered, statesNeeded);
     finalStation.add(bestStation);
   }
-  console.log(finalStation);
+
   return <div>result: {finalStation}</div>;
 };
 
@@ -42,6 +55,6 @@ function union<T>(setA: Set<T>, setB: Set<T>) {
   return new Set([...setA, ...setB]);
 }
 
-function difference<T>(setA: Set<T>, setB: Set<T>) {
+function difference<T>(setA: Set<T>, setB: Set<T>): Set<T> {
   return new Set([...setA].filter((element) => !setB.has(element)));
 }
